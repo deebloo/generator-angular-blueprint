c3308bdae86cd1328c609a3d39631f3093a403ce
Danny Blue <dblue@deloitte.com>
2015-02-12 11:57:31 -0500

    manually set path to template to avoid duplicate templates

---
+++ generators/route/index.js
@@ -0,0 +1,26 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var blueprint = require('../../lib/blueprint');
+
+module.exports = yeoman.generators.NamedBase.extend({
+  writing: writing
+});
+
+/**
+ * @name writing
+ */
+function writing() {
+  var destPath = './client/app/views/';
+
+  var tempOptions = {
+    appName: this._.camelize(this.config.get('appName')),
+    classedName: this._.classify(this.name),
+    cameledName: this._.camelize(this.name)
+  };
+
+  blueprint.copyTpl.call(this, 'view', 'html',  destPath, tempOptions);
+
+  blueprint.copyTpl.call(this, 'view', 'scss',  destPath, tempOptions);
+
+  blueprint.copyTpl.call(this, 'controller', 'js',  destPath, tempOptions);
+}
---
+++ test/route.spec.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var path = require('path');
+var assert = require('yeoman-generator').assert;
+var helpers = require('yeoman-generator').test;
+
+describe('AngularBlueprint:route', function () {
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../generators/route'))
+      .withArguments(['test'])
+      .withPrompt({ 'directory': false })
+      .withOptions({ 'skip-install': true})
+      .on('end', done);
+  });
+
+  it('creates files', function () {
+    assert.file([
+      './client/app/views/test/test.view.html',
+      './client/app/views/test/test.view.scss',
+      './client/app/views/test/test.controller.js'
+    ]);
+  });
+});
