74244aad615dce120164e66977fa1095fe7c2cd0
Danny Blue <dblue@deloitte.com>
2015-02-12 23:01:54 -0500

    create separate router files

--- test/route.spec.js
+++ test/route.spec.js
@@ -7,7 +7,7 @@ var helpers = require('yeoman-generator').test;
 describe('AngularBlueprint:route', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/route'))
-      .withArguments(['test'])
+      .withArguments(['helloWorld'])
       .withPrompt({ 'directory': false })
       .withOptions({ 'skip-install': true})
       .on('end', done);
@@ -15,10 +15,10 @@ describe('AngularBlueprint:route', function () {
 
   it('creates files', function () {
     assert.file([
-      './client/app/views/test/test.view.html',
-      './client/app/views/test/test.view.scss',
-      './client/app/views/test/test.controller.js',
-      './client/app/views/test/test.router.js'
+      './client/app/views/hello-world/hello-world.view.html',
+      './client/app/views/hello-world/hello-world.view.scss',
+      './client/app/views/hello-world/hello-world.controller.js',
+      './client/app/views/hello-world/hello-world.router.js'
     ]);
   });
 });
