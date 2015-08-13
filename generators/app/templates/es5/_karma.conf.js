857e5a6f9b8b5baad03c27826885a51ab85b64da
Danny Blue <dannybluedesign@gmail.com>
2015-08-13 08:51:48 -0400

    update es5 karma

--- generators/app/templates/es5/_karma.conf.js
+++ generators/app/templates/es5/_karma.conf.js
@@ -5,6 +5,8 @@
 // Generated on 2014-12-18 using
 // generator-karma 0.8.3
 
+var src = require('./bower.json').appPath || 'client';
+
 module.exports = function (config) {
   config.set({
     // enable / disable watching file and executing tests whenever any file changes
@@ -23,10 +25,10 @@ module.exports = function (config) {
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
-      'es5/client/*.html',
-      'es5/client/*.js',
-      'es5/client/app/{views,components}/**/*.{js,html}',
-      'es5/client/app/services/**/*.js'
+      src + '/client/*.html',
+      src + '/client/*.js',
+      src + '/client/app/{views,components}/**/*.{js,html}',
+      src + '/client/app/services/**/*.js'
     ],
 
     // list of files / patterns to exclude
